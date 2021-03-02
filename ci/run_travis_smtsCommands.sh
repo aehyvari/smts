420ca3c342fb03e0ec9debc0e5a4eb57622bdc3d
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-03-02 01:41:37 +0100

    Update run_travis_smtsCommands.sh

--- ci/run_travis_smtsCommands.sh
+++ ci/run_travis_smtsCommands.sh
@@ -2,7 +2,7 @@
 
 set -ev
 cd home/SMTS/
-mkdir build
+mkdir home/SMTS/build
 cd home/SMTS/build
 cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_CXX_FLAGS="$FLAGS" -DUSE_READLINE:BOOL=${USE_READLINE} -DCMAKE_INSTALL_PREFIX=${INSTALL} ..
 make -j4
