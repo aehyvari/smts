4b95ae29453aa12140e1004a069fcffff1616a26
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-03-05 01:07:03 +0100

    add mpi parallel job

--- ci/run_travis_opensmtCommands.sh
+++ ci/run_travis_opensmtCommands.sh
@@ -5,6 +5,6 @@ git clone https://github.com/usi-verification-and-security/opensmt.git --branch 
 cd opensmt
 mkdir build 
 cd build
-cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${INSTALL} .. 
+cmake ..
 make -j4
 make install
