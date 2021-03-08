21f3fdaf177c57ec9e117615ac9ab5f3a0dca95f
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-03-08 21:37:39 +0100

    add mpi parallel job

--- ci/run_travis_opensmtCommands.sh
+++ ci/run_travis_opensmtCommands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -ev
-cd home
+#cd home
 mkdir ${EXTERNALREPODIR}
 cd ${EXTERNALREPODIR}
 git clone https://github.com/usi-verification-and-security/opensmt.git --branch v2.0.1 --single-branch
